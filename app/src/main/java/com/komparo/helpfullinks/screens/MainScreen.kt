864a4833f10efafd2465ffa95cd4c215ac901a02
Reese <reese2brothers@gmail.com>
2024-04-09 22:08:09 +0300

    init

---
+++ app/src/main/java/com/komparo/helpfullinks/screens/MainScreen.kt
@@ -0,0 +1,14 @@
+package com.komparo.helpfullinks.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun MainScreen() {
+    Box(modifier = Modifier.fillMaxSize().background(brush = )) {
+
+    }
+}
---
+++ app/src/main/java/com/komparo/helpfullinks/screens/SevenScreen.kt
@@ -0,0 +1,8 @@
+package com.komparo.helpfullinks.screens
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SevenScreen(){
+
+}
